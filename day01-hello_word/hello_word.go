4b9192d12862ee0a8afeb0f89bc4692c3e503a9b
Mateus Siqueira <omateussiqueira@gmail.com>
2020-01-01 08:47:25 -0300

    Dia 1 - Hello World

---
+++ day01-hello_word/hello_word.go
@@ -0,0 +1,11 @@
+// Todo programa deve ter um pacote main
+package main
+
+// O pacote fmt implementa E/S formatada com funções análogas às printf e scanf de C. Pronuncie é "fumpt"
+import "fmt"
+
+// Todo programa também deve ter uma função main e será a primeira função executada.
+func main() {
+	// Println imprime "Hello World!!" na saída padrão e também quebre uma linha.
+	fmt.Println("Hello World!!")
+}
